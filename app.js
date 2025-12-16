7b7661ac61085e82c2d029429cd22696f5bd863f
house4072-cloud <house4072@gmail.com>
2025-12-16 16:34:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const sb_URL = "https://lzfksuiftgmxwkhwhnhg.sb..co";
-const sb_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6ZmtzdWlmdGdteHdraHdobmhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3NzczMDMsImV4cCI6MjA4MTM1MzMwM30.BHI8dTc18Jw3akhlRL7OZ8_0sYQwjb0-QaMGjKjUfYA";
+const supabase_URL = "https://lzfksuiftgmxwkhwhnhg.supabase..co";
+const supabase_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6ZmtzdWlmdGdteHdraHdobmhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3NzczMDMsImV4cCI6MjA4MTM1MzMwM30.BHI8dTc18Jw3akhlRL7OZ8_0sYQwjb0-QaMGjKjUfYA";
 
 const sb = supabase.createClient(
   supabase_URL,
