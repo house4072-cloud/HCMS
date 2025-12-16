29dcecf44ec35bb57a8d989c3d0b03e92183c00e
house4072-cloud <house4072@gmail.com>
2025-12-16 16:38:41 +0900

    Fix SUPABASE_URL casing in app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://lzfksuiftgmxwkhwhnhg.supabase..co";
+const SUPABASE_URL = "https://lzfksuiftgmxwkhwhnhg.SUPABASE..co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6ZmtzdWlmdGdteHdraHdobmhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3NzczMDMsImV4cCI6MjA4MTM1MzMwM30.BHI8dTc18Jw3akhlRL7OZ8_0sYQwjb0-QaMGjKjUfYA";
 
 const sb = SUPABASE.createClient(
