260071d924a4cb6aebba7f78c4006f2c57dcf1d5
house4072-cloud <house4072@gmail.com>
2025-12-16 16:41:00 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const SUPABASE_URL = "https://lzfksuiftgmxwkhwhnhg.SUPABASE..co";
+// ===== Supabase 초기화 =====
+const SUPABASE_URL = "https://lzfksuiftgmxwkhwhnhg.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6ZmtzdWlmdGdteHdraHdobmhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3NzczMDMsImV4cCI6MjA4MTM1MzMwM30.BHI8dTc18Jw3akhlRL7OZ8_0sYQwjb0-QaMGjKjUfYA";
 
-const sb = SUPABASE.createClient(
+const sb = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
