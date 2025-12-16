1a840b16d5873866adba7735fdcbb1292af4c741
house4072-cloud <house4072@gmail.com>
2025-12-16 16:38:11 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const supabase_URL = "https://lzfksuiftgmxwkhwhnhg.supabase..co";
-const supabase_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6ZmtzdWlmdGdteHdraHdobmhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3NzczMDMsImV4cCI6MjA4MTM1MzMwM30.BHI8dTc18Jw3akhlRL7OZ8_0sYQwjb0-QaMGjKjUfYA";
+const SUPABASE_URL = "https://lzfksuiftgmxwkhwhnhg.supabase..co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6ZmtzdWlmdGdteHdraHdobmhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3NzczMDMsImV4cCI6MjA4MTM1MzMwM30.BHI8dTc18Jw3akhlRL7OZ8_0sYQwjb0-QaMGjKjUfYA";
 
-const sb = supabase.createClient(
-  supabase_URL,
-  supabase_ANON_KEY
+const sb = SUPABASE.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
 
 function todayStr() {
